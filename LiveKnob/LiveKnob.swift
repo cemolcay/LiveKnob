ea12722dd3b52b08065e034680d99498510df896
cemolcay <ccemolcay@gmail.com>
2018-06-13 13:03:45 +0300

    Change default sliding sensitivity

--- LiveKnob/LiveKnob.swift
+++ LiveKnob/LiveKnob.swift
@@ -208,7 +208,7 @@ public class LiveKnobGestureRecognizer: UIPanGestureRecognizer {
   /// Horizontal and vertical slide calculation reference.
   private var lastTouchPoint: CGPoint = .zero
   /// Horizontal and vertical slide sensitivity multiplier. Defaults 0.005.
-  public var slidingSensitivity: CGFloat = 0.001
+  public var slidingSensitivity: CGFloat = 0.005
 
   // MARK: UIGestureRecognizerSubclass
 
